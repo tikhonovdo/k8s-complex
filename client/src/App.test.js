6e25a3337e340d8f33dcdaa47681fce556868cd7
Dmitry Tikhonov <dtikhonov@devexperts.com>
2020-02-28 14:56:51 +0300

    Initial commit with Kubernetes config

---
+++ client/src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  // just skip this test
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
+});
